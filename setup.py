63cb4fa73908ff59c7401041a9998d3959730b1f
Sam Devorsetz <samdevorsetz@gmail.com>
2022-06-17 11:05:08 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gradescope-ace",
+    version="0.0.1",
+    author="Sam Devorsetz",
+    author_email="samdevorsetz@gmail.com",
+    description="A tool for Gradescope's autograder",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/samdevo/gradescope-ace",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    entry_points={
+        'console_scripts': ['gradescope-ace=src.main:main'],
+    },
+    python_requires=">=3.6",
+)
